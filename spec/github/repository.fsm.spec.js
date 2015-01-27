336a4585767fef773e63b18d5e4594a48250f507
Brian Edgerton <brian.edgerton@leankit.com>
2015-01-27 17:13:00 -0600

    Repository FSM specs.

---
+++ spec/github/repository.fsm.spec.js
@@ -0,0 +1,379 @@
+var should = require( "should" );
+var sinon = require( "sinon" );
+var when = require( "when" );
+var _ = require( "lodash" );
+var machina = require( "machina" );
+var config = require( "../../src/config" );
+var store = require( "../../src/github/store.js" );
+var github = require( "../../src/github/api" )( config );
+var repoFsm = require( "../../src/github/repository.fsm" ).bind( undefined, config, github, store );
+
+var org = "myorg";
+var repo = require( "../fixtures/repos.json" )[ "from-org" ][ 0 ];
+var branch = require( "../fixtures/branch.json" ).specialbranch;
+var branchList = require( "../fixtures/branches.json" )[ "all-branches" ];
+var machineName = org + ":" + repo.name;
+
+var toIntercept = [ machineName ];
+
+var fsm;
+
+function patchTransition( machine ) {
+	machine.transitionForReal = machine.transition;
+	machine.transition = sinon.stub( machine, "transition" );
+}
+
+function startInterceptor() {
+	machina.on( "newfsm", function( machine ) {
+		if ( _.contains( toIntercept, machine.name ) ) {
+			patchTransition( machine );
+		}
+	} );
+}
+
+function stopInterceptor() {
+	machina.off( "newfsm" );
+}
+
+describe( "Repository FSM", function() {
+
+	before( function() {
+		startInterceptor();
+		fsm = repoFsm( org, repo );
+	} );
+
+	after( function() {
+		stopInterceptor();
+	} );
+
+	describe( "when getting a branch", function() {
+		var fetch;
+
+		before( function() {
+			fetch = sinon.stub( github, "fetchBranch" ).returns( when( branch ) );
+			fsm.getBranch();
+		} );
+
+		after( function() {
+			fetch.restore();
+		} );
+
+		it( "should proxy to the github api", function() {
+			fetch.getCall( 0 ).args.should.eql( [ org, repo.name, repo.default_branch ] );
+			fsm.transition.lastCall.args[ 0 ].should.equal( "fetching-tree" );
+		} );
+
+		it( "should set the main branch", function() {
+			fsm.mainBranch.should.eql( branch );
+		} );
+
+	} );
+
+	describe( "when checking for a build file", function() {
+		var originalFiles;
+		var trueFiles = [ { path: "index.js" }, { path: "config.json" }, { path: "src/nonstop.yaml" } ];
+		var falseFiles = [ { path: "index.js" }, { path: "config.json" }, { path: "server.js" } ];
+		before( function() {
+			originalFiles = fsm.files;
+		} );
+
+		after( function() {
+			fsm.files = originalFiles;
+		} );
+
+		it( "should return true if the file is present", function() {
+			fsm.files = trueFiles;
+			fsm.hasBuildFile().should.be.ok;
+		} );
+
+		it( "should return false if the file is not present", function() {
+			fsm.files = falseFiles;
+			should( fsm.hasBuildFile() ).not.be.ok;
+		} );
+	} );
+
+	describe( "on branches", function() {
+		describe( "when no data is given", function() {
+			var get;
+			before( function() {
+				get = sinon.stub( fsm, "getBranch" );
+			} );
+			after( function() {
+				get.restore();
+			} );
+			it( "should try to fetch the branch", function() {
+				fsm.onBranches( {} );
+				get.called.should.be.ok;
+			} );
+		} );
+
+		describe( "when branch data is given", function() {
+
+			before( function() {
+				fsm.mainBranch = null;
+				fsm.onBranches( { branches: branchList } );
+			} );
+
+			it( "should find and set the main branch", function() {
+				fsm.mainBranch.should.eql( branchList[ 0 ] );
+			} );
+
+			it( "should transition to fetching tree", function() {
+				fsm.transition.lastCall.args[ 0 ].should.equal( "fetching-tree" );
+			} );
+
+		} );
+	} );
+
+	describe( "when in fetching-branches state", function() {
+
+		describe( "when branches are found", function() {
+			var read;
+			var onBranches;
+			var localFsm;
+			before( function() {
+				localFsm = repoFsm( org, repo );
+				read = sinon.stub( localFsm.branches, "read" ).returns( when( branchList ) );
+				onBranches = sinon.stub( localFsm, "onBranches" );
+				localFsm.transitionForReal( "fetching-branches" );
+			} );
+
+			after( function() {
+				read.restore();
+				onBranches.restore();
+			} );
+
+			it( "should pass the data to onBranches", function( done ) {
+				_.defer( function() {
+					onBranches.getCall( 0 ).args[ 0 ].should.eql( branchList );
+					done();
+				} );
+			} );
+
+		} );
+
+		describe( "when no branches are found", function() {
+			var read;
+			var getBranch;
+			var localFsm;
+			before( function() {
+				localFsm = repoFsm( org, repo );
+				read = sinon.stub( localFsm.branches, "read" ).returns( when.reject( new Error( "Oh noes" ) ) );
+				getBranch = sinon.stub( localFsm, "getBranch" );
+				localFsm.transitionForReal( "fetching-branches" );
+			} );
+
+			after( function() {
+				read.restore();
+				getBranch.restore();
+			} );
+
+			it( "should hit the github api", function( done ) {
+				_.defer( function() {
+					getBranch.called.should.be.ok;
+					done();
+				} );
+			} );
+		} );
+	} );
+
+	describe( "when in fetching-tree state", function() {
+
+		describe( "when no tree information is available", function() {
+			var handle;
+			var localFsm;
+			before( function() {
+				localFsm = repoFsm( org, repo );
+				handle = sinon.stub( localFsm, "handle" );
+				localFsm.tree = null;
+				localFsm.mainBranch = null;
+				localFsm.transitionForReal( "fetching-tree" );
+			} );
+
+			after( function() {
+				handle.restore();
+			} );
+
+			it( "should stop", function() {
+				handle.called.should.not.be.ok;
+			} );
+
+		} );
+
+		describe( "when there is no tree but there is a main branch", function() {
+			var handle;
+			var localFsm;
+			var treeCmd;
+			var treeRead;
+			before( function() {
+				localFsm = repoFsm( org, repo );
+				localFsm.tree = null;
+				localFsm.mainBranch = {
+					commit: {
+						sha: "abc123"
+					}
+				};
+				treeRead = sinon.stub().returns( when() );
+				treeCmd = sinon.stub( localFsm, "treeCommand" ).returns( {
+					read: treeRead
+				} );
+				handle = sinon.stub( localFsm, "handle" );
+				localFsm.transitionForReal( "fetching-tree" );
+			} );
+
+			after( function() {
+				treeCmd.restore();
+				handle.restore();
+			} );
+
+			it( "should create the tree from the branch", function() {
+				treeCmd.getCall( 0 ).args.should.eql( [
+					org,
+					repo.name,
+					"abc123"
+				] );
+			} );
+
+			it( "should try to read the tree", function() {
+				treeRead.called.should.be.ok;
+			} );
+
+		} );
+
+		describe( "when the tree is available", function() {
+			var localFsm;
+			var treeData = { tree: [ "file1.js", "file2.js" ] };
+			before( function() {
+				localFsm = repoFsm( org, repo );
+				var treeRead = sinon.stub().returns( when( treeData ) );
+				localFsm.tree = { read: treeRead };
+				localFsm.transitionForReal( "fetching-tree" );
+			} );
+
+			it( "should set the repo files", function() {
+				localFsm.files.should.eql( treeData.tree );
+			} );
+
+			it( "should transition to verifying hook", function() {
+				localFsm.transition.lastCall.args[ 0 ].should.equal( "verifying-hook" );
+			} );
+		} );
+
+		describe( "when the tree read fails", function() {
+			var localFsm;
+			before( function() {
+				localFsm = repoFsm( org, repo );
+				var treeRead = sinon.stub().returns( when.reject( new Error( "Your tree is lost" ) ) );
+				localFsm.tree = { read: treeRead };
+				global.setTimeout = sinon.stub( global, "setTimeout" );
+				localFsm.transition.reset();
+				localFsm.transitionForReal( "fetching-tree" );
+			} );
+
+			after( function() {
+				global.setTimeout.restore();
+			} );
+
+			it( "should set a timer to attempt another fetch", function() {
+				var args = global.setTimeout.getCall( 0 ).args;
+				var cb = args[ 0 ];
+				args[ 1 ].should.be.greaterThan( 0 );
+				localFsm.transition.callCount.should.equal( 0 );
+
+				cb();
+				localFsm.transition.callCount.should.equal( 1 );
+				localFsm.transition.getCall( 0 ).args[ 0 ].should.equal( "fetching-tree" );
+			} );
+		} );
+
+	} );
+
+	describe( "when in verifying-hook state", function() {
+		describe( "when a build file is not present", function() {
+			var localFsm;
+			var buildFile;
+			before( function() {
+				localFsm = repoFsm( org, repo );
+				buildFile = sinon.stub( localFsm, "hasBuildFile" ).returns( false );
+				localFsm.transitionForReal( "verifying-hook" );
+			} );
+			it( "should transition to polling", function() {
+				localFsm.transition.lastCall.args[ 0 ].should.equal( "polling" );
+			} );
+
+		} );
+
+		describe( "when a build file is present", function() {
+			var localFsm;
+			var hasBuildFile;
+			describe( "when the hook is found on the repository", function() {
+				var fetchHook;
+				before( function() {
+					localFsm = repoFsm( org, repo );
+					hasBuildFile = sinon.stub( localFsm, "hasBuildFile" ).returns( true );
+					fetchHook = sinon.stub( github, "fetchHook" ).returns( when( true ) );
+					localFsm.transitionForReal( "verifying-hook" );
+				} );
+				after( function() {
+					fetchHook.restore();
+				} );
+				it( "should transition to polling", function() {
+					localFsm.hasHook.should.be.ok;
+					localFsm.transition.lastCall.args[ 0 ].should.equal( "polling" );
+				} );
+			} );
+
+			describe( "when the hook is not found on the repository", function() {
+				var fetchHook;
+				var createHook;
+				before( function() {
+					localFsm = repoFsm( org, repo );
+					hasBuildFile = sinon.stub( localFsm, "hasBuildFile" ).returns( true );
+					fetchHook = sinon.stub( github, "fetchHook" ).returns( when( false ) );
+					createHook = sinon.stub( github, "createHook" ).returns( when( true ) );
+					localFsm.hasHook = false;
+					localFsm.transitionForReal( "verifying-hook" );
+				} );
+
+				after( function() {
+					fetchHook.restore();
+					createHook.restore();
+				} );
+
+				it( "should attempt to create the webhook", function() {
+					createHook.getCall( 0 ).args.should.eql( [
+						org,
+						repo.name
+					] );
+				} );
+
+				it( "should transition to polling", function() {
+					localFsm.transition.lastCall.args[ 0 ].should.equal( "polling" );
+				} );
+			} );
+
+		} );
+
+	} );
+
+	describe( "when in the polling state", function() {
+		var localFsm;
+		before( function() {
+			localFsm = repoFsm( org, repo );
+			localFsm.hasHook = false;
+			global.setInterval = sinon.stub( global, "setInterval" );
+			localFsm.transitionForReal( "polling" );
+		} );
+		after( function() {
+			global.setInterval.restore();
+		} );
+		it( "should attempt to fetch branches on a timer", function() {
+			var args = global.setInterval.getCall( 0 ).args;
+			args[ 1 ].should.be.greaterThan( 1000 );
+			localFsm.transition.reset();
+			args[ 0 ]();
+			localFsm.transition.lastCall.args[ 0 ].should.equal( "fetching-branches" );
+		} );
+	} );
+
+} );
--- src/github/repository.fsm.js
+++ src/github/repository.fsm.js
@@ -1,29 +1,32 @@
-var config = require( "../config.js" );
-var _ = require( 'lodash' );
-var machina = require( 'machina' );
-var api = require( './api.js' )( config );
-var store = require( './store.js' );
-var branches = require( './commands/getBranches.js' )( api, store );
-var tree = require( './commands/getTree.js' )( api, store );
-var debug = require( 'debug' )( 'github:repository' );
+var _ = require( "lodash" );
+var machina = require( "machina" );
+var debug = require( "debug" )( "github:repository" );
 
-module.exports = function( organization, repository ) {
+var branchesMod = require( "./commands/getBranches.js" );
+var treeMod = require( "./commands/getTree.js" );
+
+module.exports = function( config, api, store, organization, repository ) {
+	var branches = branchesMod( api, store );
+	var tree = treeMod( api, store );
+
 	var Repository = machina.Fsm.extend( {
-		initialState: 'fetching-branches',
+		name: [ organization, repository.name ].join( ":" ),
+		initialState: "fetching-branches",
 		organization: organization,
 		repository: repository,
+		treeCommand: tree,
 		branches: branches( organization, repository.name ),
 		trees: undefined,
 		files: {},
 
 		getBranch: function() {
-			debug( 'Attempting to read default branch %s for %s - %s', repository.default_branch, organization, repository.name );
+			debug( "Attempting to read default branch %s for %s - %s", repository.default_branch, organization, repository.name );
 			api.fetchBranch( organization, repository.name, repository.default_branch )
 				.then( function( data ) {
 					this.mainBranch = data;
-					this.transition( 'fetching-tree' );
+					this.transition( "fetching-tree" );
 				}.bind( this ),
-					this.getHandle( 'branch.failed' ) );
+					this.getHandle( "branch.failed" ) );
 		},
 
 		getHandle: function( topic ) {
@@ -39,97 +42,96 @@ module.exports = function( organization, repository ) {
 		},
 
 		onBranches: function( data ) {
-			if ( data.branches ) {
-				debug( 'Fetched %d branches for %s - %s', _.keys( data.branches ).length, organization, repository.name );
+			if ( data && data.branches ) {
+				debug( "Fetched %d branches for %s - %s", _.keys( data.branches ).length, organization, repository.name );
 				this.mainBranch = _.find( data.branches, function( branch ) {
 					return branch.name == this.repository.default_branch;
 				}.bind( this ) );
-				this.transition( 'fetching-tree' );
+				this.transition( "fetching-tree" );
 			} else {
 				this.getBranch();
 			}
 		},
 
 		states: {
-			'fetching-branches': {
+			"fetching-branches": {
 				_onEnter: function() {
-					var topic = [ organization, repository.name, '#' ].join( '.' );
-					debug( 'Fetching branches for %s - %s', organization, repository.name );
+					debug( "Fetching branches for %s - %s", organization, repository.name );
 					this.branches.read()
-						.then( this.onBranches.bind( this ), this.getHandle( 'branches.failed' ) );
+						.then( this.onBranches.bind( this ), this.getHandle( "branches.failed" ) );
 				},
-				'branches.failed': function( failure ) {
-					debug( 'FAILED TO FETCH BRANCHES for %s - %s: %s', organization, repository.name, JSON.stringify( failure ) );
+				"branches.failed": function( failure ) {
+					debug( "FAILED TO FETCH BRANCHES for %s - %s: %s", organization, repository.name, JSON.stringify( failure ) );
 					this.getBranch();
 				},
-				'branch.failed': function( failure ) {
-					debug( 'FAILED TO FETCH DEFAULT BRANCH for %s - %s: %s', organization, repository.name, JSON.stringify( failure ) );
-					debug( 'Shutting down polling for repository %s - %s as it appears it has moved or has become unavailable', organization, repository.name );
+				"branch.failed": function( failure ) {
+					debug( "FAILED TO FETCH DEFAULT BRANCH for %s - %s: %s", organization, repository.name, JSON.stringify( failure ) );
+					debug( "Shutting down polling for repository %s - %s as it appears it has moved or has become unavailable", organization, repository.name );
 				}
 			},
-			'fetching-tree': {
+			"fetching-tree": {
 				_onEnter: function() {
-					debug( 'Checking %s - %s main branch for build file', organization, repository.name );
+					debug( "Checking %s - %s main branch for build file", organization, repository.name );
 					if ( !this.tree ) {
 						if ( this.mainBranch && this.mainBranch.commit.sha ) {
-							this.tree = tree( this.organization, this.repository.name, this.mainBranch.commit.sha );
+							this.tree = this.treeCommand( this.organization, this.repository.name, this.mainBranch.commit.sha );
 						} else {
-							debug( 'Main branch %s for %s - %s is showing no commit data. Cannot proceed.', repository.default_branch, organization, repository.name, this.mainBranch );
-							debug( 'Shutting down polling for repository %s - %s as it appears no commit data is available', organization, repository.name );
+							debug( "Main branch %s for %s - %s is showing no commit data. Cannot proceed.", repository.default_branch, organization, repository.name, this.mainBranch );
+							debug( "Shutting down polling for repository %s - %s as it appears no commit data is available", organization, repository.name );
 							return;
 						}
 					}
 					this.tree.read()
-						.then( this.getHandle( 'tree.fetched' ), this.getHandle( 'tree.failed' ) );
+						.then( this.getHandle( "tree.fetched" ), this.getHandle( "tree.failed" ) );
 				},
-				'tree.fetched': function( data ) {
+				"tree.fetched": function( data ) {
 					this.files = data.tree;
-					this.transition( 'verifying-hook' );
+					this.transition( "verifying-hook" );
 				},
-				'tree.failed': function( failure ) {
-					debug( 'FAILED TO FETCH TREE for %s - %s [%s]: %s', this.organization, this.repository.name, this.repository.default_branch, JSON.stringify( failure ) );
+				"tree.failed": function( failure ) {
+					debug( "FAILED TO FETCH TREE for %s - %s [%s]: %s", this.organization, this.repository.name, this.repository.default_branch, JSON.stringify( failure ) );
 					setTimeout( function() {
-						this.transition( 'fetching-tree' );
+						this.transition( "fetching-tree" );
 					}.bind( this ), 5000 );
 				}
 			},
-			'verifying-hook': {
+			"verifying-hook": {
 				_onEnter: function() {
 					if ( this.hasBuildFile() ) {
 						api.fetchHook( this.organization, this.repository.name )
 							.then( function( found ) {
 								if ( found ) {
-									this.handle( 'hook.found' );
+									this.handle( "hook.found" );
 								} else {
-									this.handle( 'hook.missing' );
+									this.handle( "hook.missing" );
 								}
-							}.bind( this ), this.getHandle( 'hook.missing' ) );
+							}.bind( this ), this.getHandle( "hook.missing" ) );
 					} else {
-						this.transition( 'polling' );
+						this.transition( "polling" );
 					}
 				},
-				'hook.missing': function() {
-					debug( 'Build file found in %s - %s, creating web hook', this.organization, this.repository.name );
+				"hook.missing": function() {
+					debug( "Build file found in %s - %s, creating web hook", this.organization, this.repository.name );
 					api.createHook( this.organization, this.repository.name )
 						.then( function() {
 							this.hasHook = true;
-							this.transition( 'polling' );
+							this.transition( "polling" );
 						}.bind( this ), function( err ) {
-								this.transition( 'polling' );
+								this.transition( "polling" );
 							}.bind( this ) );
 
 				},
-				'hook.found': function() {
-					debug( 'Webhook exists for %s - %s', this.organization, this.repository.name );
+				"hook.found": function() {
+					debug( "Webhook exists for %s - %s", this.organization, this.repository.name );
 					this.hasHook = true;
-					this.transition( 'polling' );
+					this.transition( "polling" );
 				}
 			},
-			'polling': {
+			"polling": {
 				_onEnter: function() {
 					if ( !this.hasHook ) {
 						setInterval( function() {
-							this.transition( 'fetching-branches' );
+							this.transition( "fetching-branches" );
 						}.bind( this ), 300000 );
 					}
 				}
