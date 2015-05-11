8fe9502210b9fbb5d88b0c3d204436e12ebaaab6
Brian Edgerton <brian.edgerton@leankit.com>
2015-05-11 11:33:34 -0400

    Fixed bug where server would not start due to autohost upgrade.

--- src/server.js
+++ src/server.js
@@ -2,36 +2,39 @@ var ngrok = require( 'ngrok' );
 var host = require( 'autohost' );
 var authProvider = require( 'autohost-nedb-auth' )( {} );
 var daedalus;
-
+var server;
 var config;
 
 function start() {
 	try {
-		host.init( {
+		server = host( {
 			port: config.nonstop.ci.port,
 			socketIO: true,
 			origin: 'nonstop-ci',
 			anonymous: [ '/api/commit' ]
 		}, authProvider );
+		server.start();
+
 		daedalus.register( config.nonstop.ci.port, [ '0.1.0', 'nonstop', 'ci' ] );
+
 		ngrok.connect( {
 			port: config.nonstop.ci.port,
 			authtoken: config.ngrok.token,
 			subdomain: config.ngrok.subdomain
 		}, function( err, url ) {
-				if ( err ) {
-					console.log( err, url );
-				} else {
-					var githubFsm = require( './github/main.fsm.js' );
-				}
-			} );
+			if ( err ) {
+				console.log( err, url );
+			} else {
+				var githubFsm = require( './github/main.fsm.js' );
+			}
+		} );
 	} catch (err) {
 		console.log( 'Starting server failed with', err.stack );
 	}
 }
 
 function stop() {
-	host.stop();
+	server.stop();
 }
 
 var wrapper = {
